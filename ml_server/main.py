0ae4a771035107b8cf09d30df7dec702f9faeb8a
PranshuSinglaNITD <241210092@nitdelhi.ac.in>
2026-06-19 18:41:44 +0530

    fixed cors

--- ml_server/main.py
+++ ml_server/main.py
@@ -14,7 +14,8 @@ app = FastAPI(title="JobFinder AI API")
 # 1. Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["http://localhost:3000", # Keeps your local testing working
+        "https://job-finder-blush.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
